d271f4105007c8823ffa2b2b2d3ff0ffeae3a243
VaibhavSharma <68940014+LOSTCANTiM@users.noreply.github.com>
2024-03-21 21:05:05 +0530

    Notif Interval Testing

--- main.js
+++ main.js
@@ -3,13 +3,40 @@ function Click() {
     Notification.requestPermission(function(result) {
     if (result === 'granted') {
         navigator.serviceWorker.ready.then(function(registration) {
-        registration.showNotification('Notification with ServiceWorker');
+        registration.showNotification('12 Week Year', {
+            body: "This is a test message",
+            timestamp: Math.floor(Date.now())+100,
+        });
         });
     }
     });
+    console.log(Math.floor(Date.now()));
+    console.log(Math.floor(Date.now())+10);
     // Notification.requestPermission().then(perm => {
     //     if (perm === "granted") {
     //         new Notification("Example Notification");
     //     }
     // });
 }
+
+// setInterval(function() {
+//     // check the time, if it is 10 min before end-time, display notification
+//     navigator.serviceWorker.register('sw.js');
+//     Notification.requestPermission(function(result) {
+//     if (result === 'granted') {
+//         navigator.serviceWorker.ready.then(function(registration) {
+//         registration.showNotification('12 Week Year', {
+//             body: "This is a test message",
+//             timestamp: Math.floor(Date.now())+100,
+//         });
+//         });
+//     }
+//     });
+//     console.log(Math.floor(Date.now()));
+//     console.log(Math.floor(Date.now())+10);
+//     // Notification.requestPermission().then(perm => {
+//     //     if (perm === "granted") {
+//     //         new Notification("Example Notification");
+//     //     }
+//     // });
+//  }, 1000);
